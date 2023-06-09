404586194e3deccbf96e3512c222cd855df2c589
Carsten Jahnke <carsten@jahnke.sh>
2023-06-09 16:49:42 +0200

    editing api

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,14 @@
-import { places } from '../../../lib/db';
+import Place from "../../../db/models/Place";
+import dbConnect from "../../../db/connect";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+  if (request.method === "GET") {
+    const product = await Place.findById(id);
+    if (!place) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json(place);
+  }
 }
